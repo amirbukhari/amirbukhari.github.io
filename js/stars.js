65a4040e5fc19f78181a53c190911da052c4b816
amirbukhari <amirahmedbukhari@hotmail.com>
2016-08-01 20:17:44 -0400

    js canvas image

--- js/stars.js
+++ js/stars.js
@@ -4,29 +4,50 @@ canvas.width = canvas.width = window.innerWidth;
 canvas.height = canvas.height = window.innerHeight;
 ctx.fillStyle = "#FFF";
 
-var myTimer =setInterval(draw, 100);
-var myTimer2 = setInterval(stopTimers,2500)
-var counter = 0;
-function draw()
-{
+/*var myTimer =setInterval(draw, 100);
+	var myTimer2 = setInterval(stopTimers,2500)
+	var counter = 0;
+	function draw()
+	{
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	for(i = 0; i <20; i++)
 	{
-		var x = Math.floor((Math.random() * canvas.width) + 1); 
-		var y = Math.floor((Math.random() * canvas.height) + 1); 
-		var size = Math.floor((Math.random() * 5) + 1); 
-		//ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16);
-		ctx.arc(x, y, size, 0, Math.PI*2, true);
-		ctx.fill();
-		ctx.cls();
+	var x = Math.floor((Math.random() * canvas.width) + 1); 
+	var y = Math.floor((Math.random() * canvas.height) + 1); 
+	var size = Math.floor((Math.random() * 5) + 1); 
+	//ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16);
+	ctx.arc(x, y, size, 0, Math.PI*2, true);
+	ctx.fill();
+	ctx.cls();
 	}
-
-		
 	
-}
-
-function stopTimers()
-{
+	
+	
+	}
+	
+	function stopTimers()
+	{
 	clearInterval(myTimer);
 	clearInterval(myTimer2);
-}
+}*/
+window.onload = function() {
+	// var canvas = document.getElementById("myCanvas");
+    //var context = canvas.getContext("2d");
+	var images = new Array();
+	for(i = 0 ; i < 5; i ++)
+	{
+		var imageObj = new Image();
+		images.push(imageObj);
+	
+		images[i].onload = function() {
+			ctx.drawImage(imageObj, 0, 0,canvas.width,canvas.height);
+		};
+		images[4].src = "js/whale.jpg";
+	}
+	
+	
+	//alert(images[0].src);
+	
+	
+	//alert(imageObj.src);
+};
