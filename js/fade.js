43f2ac30a3678491fed2618130fe6fdf97fb7640
Amir Bukhari <amirahmedbukhari@hotmail.com>
2016-12-11 20:05:47 -0500

    working on portfolio section

--- js/fade.js
+++ js/fade.js
@@ -1,42 +1,50 @@
 $("document").ready(function(){
     //arrange blocks in a circle
-    var block = $("#rotator div").get(),
-    increase = Math.PI * 2 / block.length,
-    x = 0, y = 0, angle = 0;
+   	 var block = $("#rotator div").get(),
+					 increase = Math.PI * 2 / block.length,
+					x = 0, y = 0, angle = 0;
 
-    for (var i = 0; i < block.length; i++) {
-        var elem = block[i];
-        x = 300 * Math.cos(angle) + 270;
-        y = 300 * Math.sin(angle) + 270;
-        elem.style.position = 'absolute';
-        elem.style.left = x + 'px';
-        elem.style.top = y + 'px';
-        var rot = 90 + (i * (360 / block.length));        
-        angle += increase;
-    }
+					for (var i = 0; i < block.length; i++) {
+						var elem = block[i];
+						x = 300 * Math.cos(angle) + 270;
+						y = 300 * Math.sin(angle) + 270;
+						elem.style.position = 'absolute';
+						elem.style.left = x + 'px';
+						elem.style.top = y + 'px';
+						var rot = 60 + (i * (360 / block.length));        
+						angle += increase;
+					}
 });
 
-
 $('*[class^="col"]').css('display','none');
-$(".btn-primary").css('display','none');
+$(".btn").css('display','none');
 $("#title h1").css('display','none');
 
 $("#title h1").slideToggle(2000);   
-$(".btn-primary").slideToggle();
+$(".btn").slideToggle();
 $(window).scroll(function(){
 	var windowWidth = $(this).width();
 	var windowHeight = $(this).height();
 	var windowScrollTop = $(this).scrollTop();
 	
-	var toFade = ["One", "Two", "Three","Four","Five"];
+	var toFade = ["One","Two" , "Three","Four","Five"];
 	
 	for( i = 0; i <toFade.length; i ++)
     {
-		if(windowScrollTop>$("#"+toFade[i]).position().top -47 && !( $("#"+toFade[i]).children().children().children(".col-md-6").hasClass("Viewed")))
+		if(windowScrollTop>$("#"+toFade[i]).position().top -(windowHeight/2) && !( $("#"+toFade[i]).children().children().children(".col-md-6").hasClass("Viewed")))
 		{
 			$("#"+toFade[i]).children().children().children('*[class^="col"]').addClass("Viewed");
 			if ($("#"+toFade[i]).children().children().children('*[class^="col"]').is(":visible") == false) {
-				$("#"+toFade[i]).children().children().children('*[class^="col"]').fadeToggle();
+				if(toFade[i] == "Two"){
+					$("#"+toFade[i]).children().children().children('*[class^="col"]').slideToggle(1500);
+					$("#"+toFade[i]).children().children().children('*[class^="col"]').attr('id','rotatorInner');
+					$("#"+toFade[i]).children().children().attr('id','rotatorOuter');
+					
+				}
+				else
+				{
+					$("#"+toFade[i]).children().children().children('*[class^="col"]').fadeToggle(1500);
+				}
 			}
 		} 
 	}
